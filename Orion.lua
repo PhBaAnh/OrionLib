87ca41b35e4c840abb1019a1ff48f1f4c0801089
Synergian <132202302+PhBaAnh@users.noreply.github.com>
2023-06-09 13:15:12 +0700

    Update Orion.lua

--- Orion.lua
+++ Orion.lua
@@ -652,7 +652,7 @@ function OrionLib:MakeWindow(WindowConfig)
 		UIHidden = true
 		OrionLib:MakeNotification({
 			Name = "Interface Hidden",
-			Content = "Tap Right Ctrl to reopen the interface",
+			Content = "Tap " .. _G.UiBind .. " to reopen the interface",
 			Time = 5
 		})
 		WindowConfig.CloseCallback()
